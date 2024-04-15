85080b87e5091724a79693d0857f53e8d045db01
DimaMen265 <menzak87@gmail.com>
2024-04-15 16:49:42 +0200

    Finished learn css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import StatusLabel, { Status } from "./components/StatusLabel";
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <div className="flex gap-5">
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      </div>
     </main>
   );
 }
